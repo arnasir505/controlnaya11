56e4956f594d961622c36d9ef60355cd9851057e
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-05-04 14:53:07 +0600

    Create register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import Appbar from './components/Appbar/Appbar';
+import NotFound from './components/NotFound/NotFound';
+import Register from './containers/Register/Register';
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <header>
+        <Appbar />
+      </header>
+      <main>
+        <Routes>
+          <Route path='/' element={'Home'} />
+          <Route path='/register' element={<Register />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </main>
+    </>
+  );
 };
 
 export default App;
