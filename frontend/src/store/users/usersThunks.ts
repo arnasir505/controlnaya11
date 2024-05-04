838775ad3e4631496a81e748938eab16904f311e
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-05-04 14:17:30 +0600

    Config redux store and app hooks, users slice and thunks, config axios

---
+++ frontend/src/store/users/usersThunks.ts
@@ -0,0 +1,65 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axiosApi from '../../axiosApi';
+import { isAxiosError } from 'axios';
+import { unsetUser } from './usersSlice';
+import { RootState } from '../../app/store';
+
+export const register = createAsyncThunk<
+  User,
+  RegisterMutation,
+  { rejectValue: ValidationError }
+>('users/register', async (registerMutation, { rejectWithValue }) => {
+  try {
+    const response = await axiosApi.post<RegisterLoginResponse>(
+      '/users',
+      registerMutation
+    );
+    return response.data.user;
+  } catch (error) {
+    if (
+      isAxiosError(error) &&
+      error.response &&
+      error.response.status === 422
+    ) {
+      return rejectWithValue(error.response.data as ValidationError);
+    }
+
+    throw error;
+  }
+});
+
+export const login = createAsyncThunk<
+  User,
+  LoginMutation,
+  { rejectValue: GlobalError }
+>('users/login', async (loginMutation, { rejectWithValue }) => {
+  try {
+    const response = await axiosApi.post<RegisterLoginResponse>(
+      '/users/sessions',
+      loginMutation
+    );
+    return response.data.user;
+  } catch (error) {
+    if (
+      isAxiosError(error) &&
+      error.response &&
+      error.response.status === 400
+    ) {
+      return rejectWithValue(error.response.data);
+    }
+
+    throw error;
+  }
+});
+
+export const logout = createAsyncThunk<void, undefined, { state: RootState }>(
+  'users/logout',
+  async (_, { getState, dispatch }) => {
+    const config = {
+      headers: { Authorization: `Bearer ${getState().users.user?.token}` },
+    };
+
+    await axiosApi.delete('/users/sessions', config);
+    dispatch(unsetUser());
+  }
+);
---
+++ frontend/src/types.d.ts
@@ -0,0 +1,48 @@
+export interface User {
+  _id: string;
+  username: string;
+  displayName: string;
+  phoneNumber: string;
+  token: string;
+}
+
+export interface RegisterMutation {
+  username: string;
+  password: string;
+  displayName: string;
+  phoneNumber: string;
+}
+
+export interface LoginMutation {
+  username: string;
+  password: string;
+}
+
+export interface ProductMutation {
+  title: string;
+  description: string;
+  image: string;
+  price: string;
+  category: string;
+}
+
+export interface RegisterLoginResponse {
+  user: User;
+  message: string;
+}
+
+export interface ValidationError {
+  errors: {
+    [key: string]: {
+      name: string;
+      message: string;
+    };
+  };
+  message: string;
+  name: string;
+  _message: string;
+}
+
+export interface GlobalError {
+  error: string;
+}
