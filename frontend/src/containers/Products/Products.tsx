4fa20fe80deae3750cec7ea6557939ad66722aa4
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-05-04 17:23:16 +0600

    Display product cards on home page

--- frontend/src/containers/Products/Products.tsx
+++ frontend/src/containers/Products/Products.tsx
@@ -1,10 +1,19 @@
 import React, { useEffect } from 'react';
-import { useAppDispatch } from '../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { fetchProducts } from '../../store/products/productsThunks';
-import { Grid } from '@mui/material';
+import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import {
+  selectProducts,
+  selectProductsLoading,
+} from '../../store/products/productsSlice';
+import Progress from '../../components/Progress/Progress';
+import { Link } from 'react-router-dom';
+import { apiUrl } from '../../constants';
 
 const Products: React.FC = () => {
   const dispatch = useAppDispatch();
+  const products = useAppSelector(selectProducts);
+  const loading = useAppSelector(selectProductsLoading);
 
   const getProducts = async () => {
     await dispatch(fetchProducts());
@@ -14,7 +23,57 @@ const Products: React.FC = () => {
     void getProducts();
   }, []);
 
-  return <Grid item xs={12} md={9}>Products</Grid>;
+  let content = <Progress />;
+
+  if (products.length > 0 && !loading) {
+    content = (
+      <Grid container spacing={2} alignItems='stretch'>
+        {products.map((product) => (
+          <Grid item key={product._id} sx={{ display: 'flex' }}>
+            <Link
+              to={`/products/${product._id}`}
+              style={{ textDecoration: 'none', display: 'flex' }}
+            >
+              <Card
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <CardMedia
+                  component={'img'}
+                  image={apiUrl + '/' + product.image}
+                  alt='img'
+                  sx={{ width: 180, height: 180, objectFit: 'contain' }}
+                />
+                <CardContent>
+                  <Typography variant='h6' color='#000'>
+                    {product.title}
+                  </Typography>
+                  <Typography variant='body1' fontWeight='bold' color='#000'>
+                    ${product.price} dollars
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+    );
+  } else if (products.length === 0 && !loading) {
+    content = (
+      <Typography variant='h5' textAlign={'center'} mt={3}>
+        No products yet.
+      </Typography>
+    );
+  }
+
+  return (
+    <Grid item xs={12} md={9}>
+      {content}
+    </Grid>
+  );
 };
 
 export default Products;
